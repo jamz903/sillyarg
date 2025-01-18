d6ebe9d766e17bcc6daba55781d3089c6e2b5c81
Xin Yi <fxy4036@gmail.com>
2025-01-19 02:37:45 +0800

    Add database endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
 import os
-from dotenv import load_dotenv
 import google.generativeai as genai
 from google.ai.generativelanguage_v1beta.types import content
+from bson import ObjectId
+import motor.motor_asyncio
+from io import StringIO
+from fastapi import FastAPI, HTTPException
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+from bson import json_util
+
 
 load_dotenv()
 GEMINI_API_KEY = os.getenv("API_KEY")
@@ -52,3 +58,19 @@ def prompt_model(name, scents, notes):
     prompt = "given these scents " + scents + " give me a list of top 5 most popular perfumes with the same " + notes + " notes, excluding " + name + ". return it to me in JSON format, with name, description, top notes, middle notes, base notes."
     response = chat_session.send_message(prompt)
     return response.to_dict()
+
+
+# Load environment variables
+load_dotenv()
+
+# MongoDB connection
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.get_database("fragrances")  # or whatever your database name is
+collection = db.get_collection("products")  # collection for your products
+
+@app.get("/all")
+async def get_all_products():
+    # Find all documents and convert to list
+    cursor = collection.find({}, {'_id': 0})  # The {'_id': 0} excludes the _id field
+    products = await cursor.to_list(length=None)
+    return products
---
+++ scraper/mongo.py
@@ -0,0 +1,37 @@
+
+import json
+import asyncio
+import motor.motor_asyncio
+import os
+from dotenv import load_dotenv
+import os
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+MONGODB_URL = os.getenv('MONGODB_URL')
+DATABASE_NAME = 'fragrance_db'
+COLLECTION_NAME ="products"
+
+file_path ="scraper\product_data.json"
+
+import json
+
+
+# Connect to MongoDB
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.get_database("fragrances")  # or whatever your database name is
+collection = db.get_collection("products")  # collection for your products
+
+# Load and insert the data
+async def insert_json_data():
+    with open("product_data.json", "r") as file:
+        data = json.load(file)
+    
+    result = await collection.insert_many(data)
+    print(f"Inserted {len(result.inserted_ids)} documents")
+
+# Run the async function
+import asyncio
+asyncio.run(insert_json_data())
